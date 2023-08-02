ff1fd2fe9305e09d66c2695f8ee603808439b219
Alexandre Leroy <alexandre.leroy@vibe.co>
2023-08-02 13:41:04 +0200

    fix: do not output any log that could give a hint fix: make process slightly less readable

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 `docker build -t buddy-go:local .`
 
-`docker run -p 8080:8080 -it buddy-go:local `
+`docker run --rm --volume /usr/share/zoneinfo:/usr/share/zoneinfo:ro -p 8080:8080 -it buddy-go:local `
 
-`ab -n 200 -c 30 'http://localhost:8080/test'`
+`ab -n 30000 -c 30 'http://localhost:8080/hello'`
--- main.go
+++ main.go
@@ -1,31 +1,80 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
-	"strconv"
-	"sync"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"syscall"
+	"time"
 )
 
-func main() {
-  http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		test()
-  })
-  http.ListenAndServe(":8080", nil)
+var zoneDirs = []string{
+	// Update path according to your OS
+	"/usr/share/zoneinfo",
+	"/usr/share/lib/zoneinfo",
+	"/usr/lib/locale/TZ",
 }
 
-func test(){
-	var wg sync.WaitGroup
-	for i := 0; i < 4000; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			f, err := os.Create("/tmp/file-" + strconv.Itoa(i))
-			if err != nil {
-				fmt.Println(err)
-			}
-			_, _ = f.WriteString("writes\n")
-		}(i)
+func GetAllTZ() []string {
+	result := make([]string, 0, 500)
+	for _, zoneDir := range zoneDirs {
+		result = append(result, listTZ(zoneDir, "")...)
 	}
+	return result
+}
+
+func listTZ(dir string, path string) []string {
+	result := make([]string, 0)
+	files, _ := os.ReadDir(dir + path)
+	for _, f := range files {
+		if f.Name() != strings.ToUpper(f.Name()[:1])+f.Name()[1:] {
+			continue
+		}
+		if f.IsDir() {
+			result = append(result, listTZ(dir, path+"/"+f.Name())...)
+		} else {
+			tz := path + "/" + f.Name()
+			if tz[0] == '/' {
+				tz = tz[1:]
+			}
+			result = append(result, tz)
+		}
+	}
+	return result
+}
+
+var tmpFolder string
+
+var allTZ = GetAllTZ()
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	id := rand.Int63()
+	fmt.Fprintf(w, "Hello, you were given id %d\n", id)
+
+	tmpFolder, err := os.MkdirTemp("/tmp/", "test")
+
+	if err != nil {
+		panic("An error occured")
+	}
+	for i := range allTZ {
+		filename := fmt.Sprintf("%s/session_data_%s.txt", tmpFolder, allTZ[i])
+		flags := syscall.O_WRONLY | syscall.O_CREAT | syscall.O_TRUNC
+		mode := uint32(0644)
+		fd, err := syscall.Open(filename, flags, mode)
+		if err == nil {
+			syscall.Write(fd, []byte(time.Now().String()))
+		}
+	}
+}
+
+func main() {
+	log.SetOutput(ioutil.Discard)
+	http.HandleFunc("/hello", hello)
+	fmt.Printf("Starting server!\n")
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
